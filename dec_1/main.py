c5df0eee545ab79bfb1c2b7c8fb3c139d0c8fe99
tfriezz <roswall.tim@gmail.com>
2024-12-02 01:11:11 +0900

    sorted lists

--- dec_1/main.py
+++ dec_1/main.py
@@ -1,7 +1,19 @@
 def main():
     with open("input.txt") as f:
         file_contents = f.read()
-        print(file_contents)
+        #print(f"input.txt: {file_contents}")
+        left_list = sorted(list(map(int, file_contents.split()[0::2])))
+        #left_list = sorted(left_list)
+        right_list = sorted(list(map(int, file_contents.split()[1::2])))
+        zipped_list = tuple(zip(left_list, right_list))
+        print(f"test: {zipped_list}")
+        """for pair in zipped_list:
+            left = int(pair[0])
+            right = int(pair[1])
+            sum = int(pair[0]) - int(pair[1])
+            print(f"{left} - {right} = {sum}")"""
+
+        
 
 
 if __name__ == "__main__":
