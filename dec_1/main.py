f22036de02515d9fbdafd21f09d7c570ff0e85c4
tfriezz <roswall.tim@gmail.com>
2024-12-02 01:27:29 +0900

    finished main function

--- dec_1/main.py
+++ dec_1/main.py
@@ -3,15 +3,18 @@ def main():
         file_contents = f.read()
         #print(f"input.txt: {file_contents}")
         left_list = sorted(list(map(int, file_contents.split()[0::2])))
-        #left_list = sorted(left_list)
         right_list = sorted(list(map(int, file_contents.split()[1::2])))
         zipped_list = tuple(zip(left_list, right_list))
-        print(f"test: {zipped_list}")
-        """for pair in zipped_list:
-            left = int(pair[0])
-            right = int(pair[1])
-            sum = int(pair[0]) - int(pair[1])
-            print(f"{left} - {right} = {sum}")"""
+        new_list = []
+        for pair in zipped_list:
+            left = (pair[0])
+            right = (pair[1])
+            if left > right:
+                new_list.append(left - right)
+            elif left < right:
+                new_list.append(right - left)
+
+        print(f"total distance: {sum(new_list)}")
 
         
 
